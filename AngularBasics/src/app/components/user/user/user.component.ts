b1e8d1ad6fbf6b3f1008d8f292ed616fb3e4bee1
Asiri Senith <asirisenith@gmail.com>
2020-12-22 23:37:05 +0530

    Check ngAfterViewInit, ngAfterViewChecked, ngOnDestory, ngDoCheck, ngAfterContentInit in Angular

--- AngularBasics/src/app/components/user/user/user.component.ts
+++ AngularBasics/src/app/components/user/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Console } from 'console';
 import { element } from 'protractor';
 
 @Component({
@@ -6,14 +7,39 @@ import { element } from 'protractor';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit, OnChanges {
+export class UserComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
   @Input() username: string;
-  @Input() name : string;
+  @Input() name: string;
 
   constructor() {
     console.log('constructor call');
   }
+
+  ngOnDestroy(): void {
+    console.log('ng OnDestroy called');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ng After View Checked called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ng After View Init called');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ng After Content Checked called')
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ng After Content Init called');
+  }
+
+  ngDoCheck(): void {
+    console.log('DoCheck called');
+  }
+
   ngOnChanges(element: SimpleChanges): void {
     console.log('ngOnChanges call');
     console.log(element);
@@ -22,7 +48,5 @@ export class UserComponent implements OnInit, OnChanges {
   ngOnInit(): void {
     console.log('ngOnInit call');
   }
-
-  
 
 }
