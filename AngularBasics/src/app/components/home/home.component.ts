4c6c1ed524ad88363f4e92a323e18eb670b79222
Asiri1996 <asirisenith@gmail.com>
2021-01-09 13:16:18 +0530

    Create our own custom Observable in rxjs. How to use observer.next option in the angular

--- AngularBasics/src/app/components/home/home.component.ts
+++ AngularBasics/src/app/components/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Data } from '@angular/router';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, OnDestroy{
+export class HomeComponent implements OnInit, OnDestroy {
 
   intervalSubscription: Subscription;
 
@@ -19,11 +19,33 @@ export class HomeComponent implements OnInit, OnDestroy{
       console.log(data);
     });
 
-    this.intervalSubscription = interval(1000).subscribe(count => {
-      console.log(count);
+    // this.intervalSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    let customeObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count > 3) {
+          observer.error('count is greater than 3');
+        }
+        if (count > 2) {
+          observer.complete();
+        }
+        count++;
+      }, 1000);
+    });
+
+    customeObservable.subscribe((data) => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+    }, () => {
+      console.log('complete');
     })
   }
-  
+
   ngOnDestroy(): void {
     this.intervalSubscription.unsubscribe();
   }
