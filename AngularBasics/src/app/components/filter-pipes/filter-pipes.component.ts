31520d09c9903beee0dac6a942a437e390d9f42d
Asiri1996 <asirisenith@gmail.com>
2021-01-12 22:40:13 +0530

    Pipes in Angular. Chaining Multiple Pipes, parameterized Pipes in the Angular

---
+++ AngularBasics/src/app/components/filter-pipes/filter-pipes.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-pipes',
+  templateUrl: './filter-pipes.component.html',
+  styleUrls: ['./filter-pipes.component.css']
+})
+export class FilterPipesComponent implements OnInit {
+
+  users = [{
+    name: 'Asiri',
+    joinedDate: new Date(12, 1, 2021)
+  },
+  {
+    name: 'Senith',
+    joinedDate: new Date(13, 1, 2021)
+  },
+  {
+    name: 'Manjitha',
+    joinedDate: new Date(14, 1, 2021)
+  }];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
