5d16d41a6ec92a83a35572dd2c5c81b786621489
Asiri1996 <asirisenith@gmail.com>
2021-01-12 20:19:24 +0530

    Create a Custom Asynchronous Validator in the Reactive Forms

--- AngularBasics/src/app/components/reactive-forms/reactive-forms.component.ts
+++ AngularBasics/src/app/components/reactive-forms/reactive-forms.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-reactive-forms',
   templateUrl: './reactive-forms.component.html',
@@ -22,7 +22,7 @@ export class ReactiveFormsComponent implements OnInit {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.isRestrictedNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.isRestrictedEmails)
       }),
       'gender': new FormControl('femail'),
       'hobbies': new FormArray([])
@@ -39,6 +39,19 @@ export class ReactiveFormsComponent implements OnInit {
     } else {
       return null;
     }
+  }
+
+  isRestrictedEmails(control: FormControl): Promise<any> | Observable<any> {
+    let promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'p.s.jayawickrama@gmail.com') {
+          resolve({ emailIsRestricted: true });
+        } else {
+          resolve(null);
+        }
+      }, 2000)
+    });
+    return promise;
   }
 
   onAddHobby() {
