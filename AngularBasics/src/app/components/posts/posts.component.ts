788711fdac062219d760a5bf2ad7f526b5a3c9ed
Asiri1996 <asirisenith@gmail.com>
2021-01-13 16:53:00 +0530

    Understanding Http Get Request and Define the types of response using interface and genaric type

--- AngularBasics/src/app/components/posts/posts.component.ts
+++ AngularBasics/src/app/components/posts/posts.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http'
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { map } from 'rxjs/Operators'
+import { Post } from './posts.model'
 
 @Component({
   selector: 'app-posts',
@@ -9,6 +11,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class PostsComponent implements OnInit {
   postForm: FormGroup
+  posts: Post[]
 
   constructor(private http: HttpClient) {}
 
@@ -17,19 +20,42 @@ export class PostsComponent implements OnInit {
       title: new FormControl(null, Validators.required),
       content: new FormControl(null, Validators.required),
     })
+    this.getPost()
+  }
+
+  getPost() {
+    this.http
+      .get<{ [key: string]: Post }>(
+        'https://angularbasic-402cf-default-rtdb.firebaseio.com/posts.json',
+      )
+      .pipe(
+        map((res) => {
+          let posts: Post[] = []
+          for (let key in res) {
+            posts.push({ ...res[key], key })
+          }
+          return posts
+        }),
+      )
+      .subscribe((res: Post[]) => {
+        console.log('inside the get method')
+        console.log(res)
+        this.posts = res
+      })
   }
 
   onCreatePost() {
     console.log(this.postForm.value)
     const postdata = this.postForm.value
     this.http
-      .post(
+      .post<{ name: string }>(
         'https://angularbasic-402cf-default-rtdb.firebaseio.com/posts.json',
         postdata,
       )
       .subscribe((res) => {
         console.log('inside the response')
         console.log(res)
+        this.getPost()
       })
   }
 }
