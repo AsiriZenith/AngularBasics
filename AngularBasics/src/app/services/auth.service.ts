37dde5b9957104bb8b09640bedfeb2710f973642
Asiri1996 <asirisenith@gmail.com>
2021-01-15 21:53:10 +0530

    Authentication - Improve Error Messages with Catch Error and Throw Error rxjs Operator

--- AngularBasics/src/app/services/auth.service.ts
+++ AngularBasics/src/app/services/auth.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
+import { throwError } from 'rxjs'
+import { catchError } from 'rxjs/Operators'
 import { AuthResponseVM } from '../components/auth/auth.responsedata'
 
 @Injectable({ providedIn: 'root' })
@@ -9,10 +11,24 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signUp(email: string, password: string) {
-    return this.http.post<AuthResponseVM>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBYa9Cu7wvSPPxvArRbjYN66Hz4bBs6_kM`,
-      { email, password, returnSecureToken: true },
-    )
+    return this.http
+      .post<AuthResponseVM>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBYa9Cu7wvSPPxvArRbjYN66Hz4bBs6_kM`,
+        { email, password, returnSecureToken: true },
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMessage = 'An error occured!!'
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage)
+          }
+          switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'Email Already Exists'
+          }
+          return throwError(errorMessage)
+        }),
+      )
   }
 
   login() {
